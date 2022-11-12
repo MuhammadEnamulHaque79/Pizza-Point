d8c84cfa296cc9402184c5b23d3dcec34fbcfa2e
Enamul <muhammad.enamul.haque79@gmail.com>
2022-11-12 11:41:45 -0800

    yes,i can do

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,7 +5,8 @@ const Service = ({ service }) => {
     const { img, name, price, description } = service;
     return (
         <div className='service-container'>
-            <img style={{ width: '385px', height: '385px' }} src={img} alt="" />
+            
+            <img style={{ width: '400px', height: '385px' }} src={img} alt="" />
             <h3>Name : {name}</h3>
             <p>price : {price}</p>
             <p>description : {description}</p>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,25 +3,28 @@ import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
-   const[services,setServices]=useState({});
-   useEffect(()=>{
-    fetch('services.json')
-        .then(res => res.json())
-        // .then(data => console.log(data))
-        .then(data => setServices(data))
-   },[]);
+    const [services, setServices] = useState({});
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            // .then(data => console.log(data))
+            .then(data => setServices(data))
+    }, []);
     return (
-        <div className='container'>
-            {/* <h3> services:{services.length}</h3> */}
-            {
-                services.map(service => <Service
-                key={service.id}
-                service={service}
-                ></Service>)
-            }
-          
+        <div>
+            <h2 className='text-success text-center mt-3'>Our Services</h2>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
 
 export default Services;
+
+
