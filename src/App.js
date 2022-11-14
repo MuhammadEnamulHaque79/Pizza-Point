9068d479ca0c841f4f10901edf9771b1debf4f31
Enamul <muhammad.enamul.haque79@gmail.com>
2022-11-14 12:39:54 -0800

    toggle added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from './Pages/About/About';
-
-
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import Home from './Pages/Home/Home/Home';
@@ -10,6 +8,8 @@ import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import Checkout from './Pages/Checkout/Checkout/Checkout';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 
 
 function App() {
@@ -23,6 +23,11 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
@@ -31,5 +36,5 @@ function App() {
 }
 
 export default App;
-    
-      
+
+
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
-    const{serviceId}=useParams();
+    const { serviceId } = useParams();
     return (
         <div>
             <h3>Detail of service :{serviceId}</h3>
+            <div className='text-center'>
+                <Link to="/checkout">
+                    <button className='btn btn-primary'>Checkout Procceed</button>
+                </Link>
+            </div>
         </div>
     );
 };
