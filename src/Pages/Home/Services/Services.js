bfcfa4cd3bb27aeb9a0bc657a174d4e88f138e00
Enamul <muhammad.enamul.haque79@gmail.com>
2022-11-20 20:00:01 +0600

    updated

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -10,42 +10,42 @@ const services=[
      "name":"vapha pitha",
      "price":10,
      "description":"lorem25",
-     "img":"https://i.ibb.co/Z84vN99/img1.png"
+     "img":"https://i.ibb.co/FKPJyLv/1.jpg"
     },
     {
      "id":2,
      "name":"vapha pitha",
      "price":10,
      "description":"lorem25",
-     "img":"https://i.ibb.co/Z84vN99/img1.png"
+     "img":"https://i.ibb.co/grQJ94s/2.jpg"
     },
     {
      "id":3,
      "name":"vapha pitha",
      "price":10,
      "description":"lorem25",
-     "img":"https://i.ibb.co/Z84vN99/img1.png"
+     "img":"https://i.ibb.co/DVqF16X/3.jpg"
     },
     {
      "id":4,
      "name":"vapha pitha",
      "price":10,
      "description":"lorem25",
-     "img":"https://i.ibb.co/Z84vN99/img1.png"
+     "img":"https://i.ibb.co/xzxpDzV/4.jpg"
     },
     {
      "id":5,
      "name":"vapha pitha",
      "price":10,
      "description":"lorem25",
-     "img":"https://i.ibb.co/Z84vN99/img1.png"
+     "img":"https://i.ibb.co/5847fmb/5.jpg"
     },
     {
      "id":6,
      "name":"vapha pitha",
      "price":10,
      "description":"lorem25",
-     "img":"https://i.ibb.co/Z84vN99/img1.png"
+     "img":"https://i.ibb.co/wY46cz4/6.jpg"
     }
    
  ]
