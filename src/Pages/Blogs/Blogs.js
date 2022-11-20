b1c5971036b69f992edf8785ee347087b7f57363
Enamul <muhammad.enamul.haque79@gmail.com>
2022-11-20 20:55:09 +0600

    blogs edited

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -8,26 +8,39 @@ const Blogs = () => {
         <div className='blogs-container'>
             <h3 className='text-center'>My Blogs Q & A !!</h3>
             <div className='blogs-container1'>
-            
+
                 <h5>13.1 Write difference between authorization and authentication?</h5>
-                
-                <p style={{marginLeft:'15px'}}><i class="fa-solid fa-pen-to-square"></i>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, deserunt nihil dolore provident amet at maiores quas sapiente illum corrupti eveniet explicabo porro officia numquam tempora quam hic eum tempore consequuntur consectetur! Voluptas ad ipsa nemo doloribus consectetur nam nesciunt voluptatem consequatur dicta soluta sequi, rem numquam autem corrupti? Excepturi porro dolores voluptatem necessitatibus eum in consequuntur earum sint explicabo ipsa omnis possimus, officia amet fugit delectus! Quidem alias laudantium error sequi placeat libero mollitia cupiditate laborum adipisci, nesciunt quibusdam distinctio labore modi consequuntur eligendi quis totam aliquid aperiam, veniam, expedita neque deserunt consequatur ullam. Laboriosam, velit! Ducimus, exercitationem dolorum. </p>
-                
+
+                <p style={{ marginLeft: '15px' }}><i class="fa-solid fa-pen-to-square"></i>
+                    Authentication is a process by which you provide proofs that you are who you claim to be. Authorization is granting you valid permissions
+                </p>
+
             </div>
 
             <div className='blogs-container2'>
                 <h5>13.2 Why are you using firebase? What other options do you have to implement authentication?</h5>
-                <p style={{marginLeft:'15px'}}><i class="fa-solid fa-pen-to-square"></i>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident rerum cumque debitis nulla sequi alias ducimus voluptates natus aliquam possimus eos laudantium voluptatum vitae, magni fugit, exercitationem quam minus doloremque delectus reprehenderit non blanditiis soluta optio. Obcaecati ipsam excepturi, ullam debitis tenetur necessitatibus, beatae veritatis quia quo fuga porro dolorem, eius ratione. Quasi est aspernatur molestias repellat sequi optio ex a officia maxime veniam, quae eaque natus voluptas voluptatibus accusantium odit, ipsum animi esse error nulla. Accusantium sequi, quae, laudantium adipisci culpa a consequuntur placeat quis id temporibus quam, tenetur non illo laboriosam facere ratione eum quibusdam accusamus optio neque?</p>
+                <p style={{ marginLeft: '15px' }}><i class="fa-solid fa-pen-to-square"></i>
+                    Firebase help me develop high-quality apps, grow your user base,
+                    The Firebase Realtime Database lets me build rich, collaborative applications by allowing secure access to the database directly from client-side code.
+                    Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.
+                </p>
 
             </div>
 
             <div className='blogs-container3'>
-                
+
                 <h5>13.3 What other services does firebase provide other than authentication?</h5>
-                <p style={{marginLeft:'15px'}}><i class="fa-solid fa-pen-to-square"></i>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea laboriosam beatae ex recusandae earum soluta minima assumenda nulla quos voluptas nostrum ipsam, illo doloribus, harum dignissimos repudiandae inventore dolorem reiciendis omnis sapiente veniam consectetur voluptatibus! Architecto impedit sapiente sequi aspernatur autem? Facere asperiores fugiat pariatur in architecto deleniti, nihil cupiditate neque laborum velit doloribus voluptates ad, exercitationem ducimus obcaecati distinctio quae aut temporibus quidem debitis, ab non. Quos et iure possimus exercitationem! Ab laborum est aut dolores nihil aliquam aperiam officia labore voluptas, quidem sequi deserunt nesciunt! Magni, voluptatum corrupti fugit possimus veniam accusamus eos beatae provident aliquid porro.</p>
+                <p style={{ marginLeft: '15px' }}><i class="fa-solid fa-pen-to-square"></i>
+                    There are many services which Firebase provides, Most useful of them are:
+                    Cloud Firestore.
+                    Cloud Functions.
+                    Authentication.
+                    Hosting.
+                    Cloud Storage.
+                    Google Analytics.
+                    Predictions.
+                    Cloud Messaging.
+                </p>
 
             </div>
         </div>
